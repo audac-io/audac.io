74ce2e5172b238fc03020888ab0dbd8d0a6ef877
rob thijssen <rthijssen@gmail.com>
2022-09-15 15:51:40 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from 'react';
+import {
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+import Home from './Home';
+import Identity from './Identity';
 
 function App() {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/identity">identity</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/identity" element={ <Identity /> } />
+      </Routes>
     </div>
   );
 }
---
+++ src/Identity.js
@@ -0,0 +1,10 @@
+
+function Identity() {
+  return (
+    <div>
+      <h2>identity</h2>
+    </div>
+  );
+}
+
+export default Identity;
